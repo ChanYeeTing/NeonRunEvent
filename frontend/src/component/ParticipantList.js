0a4b2795d87a98ececd7a03487cc960e2253dd16
ChanYeeTing <yeeting@student.usm.my>
2024-12-02 22:35:20 +0800

    view paymentFile

--- frontend/src/component/ParticipantList.js
+++ frontend/src/component/ParticipantList.js
@@ -3,22 +3,23 @@ import './ParticipantList.css';
 
 function ParticipantList() {
     const initialParticipants = [
-        { id: 1, name: "John", icNumber: "010912-04-0143", contactNo: "011-3489028", category: "Student USM", matricNo: "157329", package: "B", tshirtSize: "M", paymentFile: "View", status: "Approved" },
-    { id: 2, name: "Jane", icNumber: "050214-08-1321", contactNo: "012-6453243", category: "Student USM", matricNo: "158342", package: "A", tshirtSize: "N/A", paymentFile: "View", status: "Approved" },
-    { id: 3, name: "Mike", icNumber: "011021-06-0143", contactNo: "018-3234324", category: "Public", matricNo: "N/A", package: "B", tshirtSize: "S", paymentFile: "View", status: "Approved" },
-    { id: 4, name: "Emily", icNumber: "020502-04-0441", contactNo: "011-6754523", category: "Public", matricNo: "N/A", package: "B", tshirtSize: "M", paymentFile: "View", status: "Approved" },
-    { id: 5, name: "Chris", icNumber: "031109-08-1274", contactNo: "011-78973242", category: "Student USM", matricNo: "157453", package: "B", tshirtSize: "XL", paymentFile: "View", status: "Approved" },
-    { id: 6, name: "Sarah", icNumber: "990816-10-1432", contactNo: "016-9823742", category: "Public", matricNo: "N/A", package: "C", tshirtSize: "L", paymentFile: "View", status: "Pending" },
-    { id: 7, name: "Ali", icNumber: "950324-07-1321", contactNo: "013-4567890", category: "Student USM", matricNo: "157894", package: "A", tshirtSize: "S", paymentFile: "View", status: "Pending" },
-    { id: 8, name: "Siti", icNumber: "000412-05-0123", contactNo: "017-8923748", category: "Student USM", matricNo: "158674", package: "C", tshirtSize: "M", paymentFile: "View", status: "Pending" },
-    { id: 9, name: "Tom", icNumber: "021223-09-8765", contactNo: "019-7283945", category: "Public", matricNo: "N/A", package: "B", tshirtSize: "L", paymentFile: "View", status: "Pending" },
-    { id: 10, name: "Jessica", icNumber: "030101-02-5432", contactNo: "012-7894321", category: "Public", matricNo: "N/A", package: "A", tshirtSize: "S", paymentFile: "View", status: "Pending" },
+        { id: 1, name: "John", icNumber: "010912-04-0143", contactNo: "011-3489028", category: "Student USM", matricNo: "157329", package: "B", tshirtSize: "M", paymentFile: "/images/payment1.jpg", status: "Approved" },
+    { id: 2, name: "Jane", icNumber: "050214-08-1321", contactNo: "012-6453243", category: "Student USM", matricNo: "158342", package: "A", tshirtSize: "N/A", paymentFile: "/images/payment1.jpg", status: "Approved" },
+    { id: 3, name: "Mike", icNumber: "011021-06-0143", contactNo: "018-3234324", category: "Public", matricNo: "N/A", package: "B", tshirtSize: "S", paymentFile: "/images/payment1.jpg", status: "Approved" },
+    { id: 4, name: "Emily", icNumber: "020502-04-0441", contactNo: "011-6754523", category: "Public", matricNo: "N/A", package: "B", tshirtSize: "M", paymentFile: "/images/payment1.jpg", status: "Approved" },
+    { id: 5, name: "Chris", icNumber: "031109-08-1274", contactNo: "011-78973242", category: "Student USM", matricNo: "157453", package: "B", tshirtSize: "XL", paymentFile: "/images/payment1.jpg", status: "Approved" },
+    { id: 6, name: "Sarah", icNumber: "990816-10-1432", contactNo: "016-9823742", category: "Public", matricNo: "N/A", package: "C", tshirtSize: "L", paymentFile: "/images/payment1.jpg", status: "Pending" },
+    { id: 7, name: "Ali", icNumber: "950324-07-1321", contactNo: "013-4567890", category: "Student USM", matricNo: "157894", package: "A", tshirtSize: "S", paymentFile: "/images/payment1.jpg", status: "Pending" },
+    { id: 8, name: "Siti", icNumber: "000412-05-0123", contactNo: "017-8923748", category: "Student USM", matricNo: "158674", package: "C", tshirtSize: "M", paymentFile: "/images/payment1.jpg", status: "Pending" },
+    { id: 9, name: "Tom", icNumber: "021223-09-8765", contactNo: "019-7283945", category: "Public", matricNo: "N/A", package: "B", tshirtSize: "L", paymentFile: "/images/payment1.jpg", status: "Pending" },
+    { id: 10, name: "Jessica", icNumber: "030101-02-5432", contactNo: "012-7894321", category: "Public", matricNo: "N/A", package: "A", tshirtSize: "S", paymentFile: "/images/payment1.jpg", status: "Pending" },
     ]
 
     const [participants, setParticipants] = useState(initialParticipants);
     const [currentTab, setCurrentTab] = useState("Pending");
     const [packageFilter, setPackageFilter] = useState('');
     const [categoryFilter, setCategoryFilter] = useState('');
+    const [modalImage, setModalImage] = useState(null);
 
     // Filter participants based on category and package
     const filteredParticipants = participants.filter(participant => {
@@ -48,6 +49,15 @@ function ParticipantList() {
     // Separate participants by status
     const pendingParticipants = filteredParticipants.filter(p => p.status === "Pending");
     const ApprovedParticipants = filteredParticipants.filter(p => p.status === "Approved");
+
+    const openModal = (image) => {
+        setModalImage(image);
+    };
+
+    // Handle closing the modal
+    const closeModal = () => {
+        setModalImage(null);
+    };
 
     return (
         <div className="list-container">
@@ -122,7 +132,12 @@ function ParticipantList() {
                                     <td>{participant.matricNo}</td>
                                     <td>{participant.package}</td>
                                     <td>{participant.tshirtSize}</td>
-                                    <td>{participant.paymentFile}</td>
+                                    <td>
+                                        {console.log(participant.name+participant.paymentFile)}
+                                    <button onClick={() => openModal(participant.paymentFile)}>
+                                            View
+                                    </button>
+                                    </td>
                                     <td>
                                         <div className="action-container">
                                         <button className="accept-button" onClick={() => handleAccept(participant.id)}>Accept</button>
@@ -166,6 +181,17 @@ function ParticipantList() {
                             ))}
                         </tbody>
                     </table>
+                </div>
+            )}
+
+            {modalImage && (
+                <div className="modal" onClick={closeModal}>
+                    <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                        <img src={modalImage} alt="Payment Proof" className="modal-image" />
+                        <div>
+                            <button onClick={closeModal}>Close</button>
+                        </div>
+                    </div>
                 </div>
             )}
         </div>
